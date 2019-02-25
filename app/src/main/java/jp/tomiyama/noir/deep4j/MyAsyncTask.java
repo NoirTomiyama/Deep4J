67cefbc50a9e9960c49e717b34179e39a6f8ec14
Ryota Tomiyama <lit.karu.noir@gmail.com>
2019-02-25 18:34:52 +0900

    first commit

---
+++ app/src/main/java/jp/tomiyama/noir/deep4j/MyAsyncTask.java
@@ -0,0 +1,109 @@
+package jp.tomiyama.noir.deep4j;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+
+public class MyAsyncTask extends AsyncTask {
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+
+
+        createAndUseNetwork();
+
+        return null;
+    }
+
+    private void createAndUseNetwork() {
+
+        Log.d("createAndNetwork","start");
+
+
+        DenseLayer inputLayer = new DenseLayer.Builder()
+                .nIn(2)
+                .nOut(3)
+                .name("Input")
+                .build();
+
+        DenseLayer hiddenLayer = new DenseLayer.Builder()
+                .nIn(3)
+                .nOut(2)
+                .name("Hidden")
+                .build();
+
+        OutputLayer outputLayer = new OutputLayer.Builder()
+                .nIn(2)
+                .nOut(2)
+                .name("Output")
+                .activation(Activation.SOFTMAX)
+                .build();
+
+        NeuralNetConfiguration.Builder nncBuilder = new NeuralNetConfiguration.Builder();
+        nncBuilder.iterations(60000);
+        nncBuilder.learningRate(0.1);
+
+        NeuralNetConfiguration.ListBuilder listBuilder = nncBuilder.list();
+        listBuilder.layer(0, inputLayer);
+        listBuilder.layer(1, hiddenLayer);
+        listBuilder.layer(2, outputLayer);
+
+        listBuilder.backprop(true);
+
+        MultiLayerNetwork myNetwork = new MultiLayerNetwork(listBuilder.build());
+        myNetwork.init();
+
+        final int NUM_SAMPLES = 4;
+
+        INDArray trainingInputs = Nd4j.zeros(NUM_SAMPLES, inputLayer.getNIn());
+        INDArray trainingOutputs = Nd4j.zeros(NUM_SAMPLES, outputLayer.getNOut());
+
+        // If 0,0 show 0
+        trainingInputs.putScalar(new int[]{0,0}, 0);
+        trainingInputs.putScalar(new int[]{0,1}, 0);
+        trainingOutputs.putScalar(new int[]{0,0}, 0);
+        trainingOutputs.putScalar(new int[]{0,1}, 1);
+
+        // If 0,1 show 1
+        trainingInputs.putScalar(new int[]{1,0}, 0);
+        trainingInputs.putScalar(new int[]{1,1}, 1);
+        trainingOutputs.putScalar(new int[]{1,0}, 1);
+        trainingOutputs.putScalar(new int[]{1,1}, 0);
+
+        // If 1,0 show 1
+        trainingInputs.putScalar(new int[]{2,0}, 1);
+        trainingInputs.putScalar(new int[]{2,1}, 0);
+        trainingOutputs.putScalar(new int[]{2,0}, 1);
+        trainingOutputs.putScalar(new int[]{2,1}, 0);
+
+        // If 1,1 show 0
+        trainingInputs.putScalar(new int[]{3,0}, 1);
+        trainingInputs.putScalar(new int[]{3,1}, 1);
+        trainingOutputs.putScalar(new int[]{3,0}, 0);
+        trainingOutputs.putScalar(new int[]{3,1}, 1);
+
+        DataSet myData = new DataSet(trainingInputs, trainingOutputs);
+        myNetwork.fit(myData);
+
+        // Create input
+        INDArray actualInput = Nd4j.zeros(1,2);
+        actualInput.putScalar(new int[]{0,0}, 1);
+        actualInput.putScalar(new int[]{0,1}, 1);
+
+        // Generate output
+        INDArray actualOutput = myNetwork.output(actualInput);
+
+        Log.d("myNetwork Output", actualOutput.toString());
+
+        Log.d("createAndNetwork","end");
+
+    }
+}
